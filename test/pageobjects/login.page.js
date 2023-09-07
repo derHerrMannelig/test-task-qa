3a2cff08f113aab36c82787d67ffbc157b2e4ded
Ilya Tyupo <derherrmannelig@gmail.com>
2023-09-07 14:38:41 +0300

    fix: add assertions where needed; migrate valid login code snippet to login-page file as method

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,5 +1,6 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js'
+import { browser } from '@wdio/globals'
 import assert from 'node:assert'
 
 class LoginPage extends Page {
@@ -36,16 +37,32 @@ class LoginPage extends Page {
     async loginClick (){
         await this.btnSubmit.click();
     }
+    async loginValid (){
+        // check of current webpage
+        const page = await browser.getUrl();
+        assert.strictEqual(page, 'https://www.saucedemo.com/inventory.html');
+        console.log(`Redirected to: ${page}`);
+        // check of displayed elements
+        const products = await $('div.inventory_list');
+        const cart = await $('a.shopping_cart_link');
+        let productsDisplayed = await products.isDisplayed();
+        let cartDisplayed = await cart.isDisplayed();
+        assert.strictEqual(productsDisplayed, true);
+        assert.strictEqual(cartDisplayed, true);
+        console.log(`Products are displayed: ${productsDisplayed}\nCart is displayed: ${cartDisplayed}`);
+    }
     async loginError (){
-        // console check of error icons
+        // check of error icons
         const icons = await $('svg.error_icon');
         let iconsDisplayed = await icons.isDisplayed();
+        assert.strictEqual(iconsDisplayed, true);
         console.log(`"X" icons are displayed on the "Login" and "Password" fields: ${iconsDisplayed}`);
-        // console check of highlights
+        // check of highlights
         const highlight = await $('input.input_error.error');
         let highlightExist = await highlight.isExisting();
+        assert.strictEqual(highlightExist, true);
         console.log(`Red highlights are applied to text fields: ${highlightExist}`);
-        // strict check of error message
+        // check of error message
         const error = await $('h3[data-test="error"]');
         let errorText = await error.getText();
         assert.strictEqual(errorText, 'Epic sadface: Username and password do not match any user in this service');
--- test/specs/test-case1.js
+++ test/specs/test-case1.js
@@ -12,15 +12,7 @@ describe('Valid Login', () => {
     })
     it('should on click redirect user to inventory page, products and cart are displayed', async () => {
         await LoginPage.loginClick();
-        // console check of current webpage
-        const page = await browser.getUrl();
-        console.log(`Redirected to: ${page}`);
-        // console check of displayed elements
-        const products = await $('div.inventory_list');
-        const cart = await $('a.shopping_cart_link');
-        let productsDisplayed = await products.isDisplayed();
-        let cartDisplayed = await cart.isDisplayed();
-        console.log(`Products are displayed: ${productsDisplayed}\nCart is displayed: ${cartDisplayed}`);
+        await LoginPage.loginValid();
         await browser.pause(1000);
     })
 })
