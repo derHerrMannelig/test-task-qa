2c530c23d9c288cf65d84f9800eadb188271de87
Ilya Tyupo <derherrmannelig@gmail.com>
2023-09-11 15:39:31 +0300

    feat: add test-case6

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -26,6 +26,24 @@ class InventoryPage extends Page {
     get inventoryItems () {
         return $$('div.inventory_item_name')
     }
+    get inventoryPrices () {
+        return $$('div.inventory_item_price')
+    }
+    get btnSort () {
+        return $('select.product_sort_container')
+    }
+    get btnAZ () {
+        return $('option[value="az"]')
+    }
+    get btnZA () {
+        return $('option[value="za"]')
+    }
+    get btnLoHi () {
+        return $('option[value="lohi"]')
+    }
+    get btnHiLo () {
+        return $('option[value="hilo"]')
+    }
 
     async burgerClick (){
         await this.btnBurger.click();
@@ -95,6 +113,87 @@ class InventoryPage extends Page {
         let itemPendingName = await itemPending.getText();
         assert.strictEqual(itemPendingName, this.invItem);
         console.log(`Item is the same: ${itemPendingName}`);
+    }
+    async sortClick () {
+        await this.btnSort.click();
+    }
+    async sortByName (button) {
+        // initial array of item names as reference
+        let arrayName = [];
+        let inventoryItems = await this.inventoryItems;
+        for (const inventoryItem of inventoryItems) {
+            let arrayItem = await inventoryItem.getText();
+            arrayName.push(arrayItem);
+        }
+        // checks of correct sorting by name
+        if (button === 'A-Z') {
+            arrayName.sort();
+            await this.btnAZ.click();
+            let az = [];
+            let inventoryItems = await this.inventoryItems;
+            for (const inventoryItem of inventoryItems) {
+                let azItem = await inventoryItem.getText();
+                az.push(azItem);
+            }
+            assert.strictEqual(JSON.stringify(az), JSON.stringify(arrayName));
+            console.log(`Items are sorted A-Z: ${az.join(', ')}`)
+        } else if (button === 'Z-A') {
+            arrayName.sort((a, b) => b.localeCompare(a));
+            await this.btnZA.click();
+            let za = [];
+            let inventoryItems = await this.inventoryItems;
+            for (const inventoryItem of inventoryItems) {
+                let zaItem = await inventoryItem.getText();
+                za.push(zaItem);
+            }
+            assert.strictEqual(JSON.stringify(za), JSON.stringify(arrayName));
+            console.log(`Items are sorted Z-A: ${za.join(', ')}`)
+        } else {
+            throw new Error("Specify correct sorting option! ('A-Z' or 'Z-A')");
+        }
+    }
+    async sortByPrice (button) {
+        // initial array of item prices as reference
+        let arrayPrice = [];
+        let inventoryPrices = await this.inventoryPrices;
+        for (const inventoryPrice of inventoryPrices) {
+            let arrayItemPrice = await inventoryPrice.getText();
+            arrayPrice.push(arrayItemPrice);
+        }
+        // checks of correct sorting by price
+        if (button === 'low-high') {
+            arrayPrice.sort((a, b) => {
+                const priceA = parseFloat(a.slice(1));
+                const priceB = parseFloat(b.slice(1));
+                return priceA - priceB;
+            });
+            await this.btnLoHi.click();
+            let lohi = [];
+            let inventoryPrices = await this.inventoryPrices;
+            for (const inventoryPrice of inventoryPrices) {
+                let lohiItem = await inventoryPrice.getText();
+                lohi.push(lohiItem);
+            }
+            assert.strictEqual(JSON.stringify(lohi), JSON.stringify(arrayPrice));
+            console.log(`Items are sorted low-high: ${lohi.join(', ')}`)
+        } else if (button === 'high-low') {
+            arrayPrice.sort((a, b) => {
+                const priceA = parseFloat(a.slice(1));
+                const priceB = parseFloat(b.slice(1));
+                return priceB - priceA;
+            });
+            await this.btnHiLo.click();
+            let hilo = [];
+            let inventoryPrices = await this.inventoryPrices;
+            for (const inventoryPrice of inventoryPrices) {
+                let hiloItem = await inventoryPrice.getText();
+                hilo.push(hiloItem);
+            }
+            assert.strictEqual(JSON.stringify(hilo), JSON.stringify(arrayPrice));
+            console.log(`Items are sorted high-low: ${hilo.join(', ')}`)
+        } else {
+            throw new Error("Specify correct sorting option! ('low-high' or 'high-low')");
+        }
     }
 }
 
